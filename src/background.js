784f3b410261d2b1f26a05eb080a7b478abedfc3
Yura Gerassimenko <Yuriy.Gerasimenko@tatar.ru>
2016-02-16 13:05:57 +0300

    first commit

---
+++ src/background.js
@@ -0,0 +1,47 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function (details) {
+        return redirect(details);
+    },
+    {
+        urls: [
+            "https://api.asm.skype.com/*",
+            "https://login.skype.com/*"
+        ],
+        types: ['main_frame']
+    },
+    ['blocking']
+);
+
+function redirect(details) {
+    var pattern, from, to, match;
+
+    var regexList = [
+        [
+            'https://api.asm.skype.com/s/i\\?(.*)',
+            'https://api.asm.skype.com/v1/objects/$1/views/imgpsh_fullsize'
+        ],
+        [
+            'https://login.skype.com/login/sso\\?go=xmmfallback\\?pic=(.*)',
+            'https://api.asm.skype.com/v1/objects/$1/views/imgpsh_fullsize'
+        ]
+    ];
+
+    for (var i = 0; i < regexList.length; ++i) {
+
+        from = regexList[i][0];
+        to   = regexList[i][1];
+
+        pattern = new RegExp(from, 'ig');
+        match   = details.url.match(pattern);
+
+        if (match) {
+            var redirectUrl = details.url.replace(pattern, to);
+            if (redirectUrl != details.url) {
+
+                return {redirectUrl: redirectUrl};
+            }
+        }
+    }
+
+    return {};
+}
