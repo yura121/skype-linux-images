c7bdb63019717fd745fb5acdb0e2de198ab1c721
Yura Gerassimenko <Yuriy.Gerasimenko@tatar.ru>
2016-02-29 09:50:07 +0300

    Добавил редирект на web skype, если пользователь еще не авторизовался (http-код 401)

--- src/background.js
+++ src/background.js
@@ -1,5 +1,6 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function (details) {
+        
         return redirect(details);
     },
     {
@@ -7,14 +8,37 @@ chrome.webRequest.onBeforeRequest.addListener(
             "https://api.asm.skype.com/*",
             "https://login.skype.com/*"
         ],
-        types: ['main_frame']
+        types: [
+            'main_frame'
+        ]
     },
-    ['blocking']
+    [
+        'blocking'
+    ]
+);
+
+chrome.webRequest.onHeadersReceived.addListener(
+    function (details) {
+        if (extractStatusCode(details.statusLine) == 401) {
+            
+            return {
+                redirectUrl: "https://web.skype.com/"
+            };
+        }
+    },
+    {
+        urls: [
+            "https://api.asm.skype.com/*"
+        ]
+    },
+    [
+        "blocking"
+    ]
 );
 
 function redirect(details) {
     var pattern, from, to, match;
-
+    
     var regexList = [
         [
             'https://api.asm.skype.com/s/i\\?(.*)',
@@ -25,23 +49,36 @@ function redirect(details) {
             'https://api.asm.skype.com/v1/objects/$1/views/imgpsh_fullsize'
         ]
     ];
-
+    
     for (var i = 0; i < regexList.length; ++i) {
-
+        
         from = regexList[i][0];
         to   = regexList[i][1];
-
+        
         pattern = new RegExp(from, 'ig');
         match   = details.url.match(pattern);
-
+        
         if (match) {
             var redirectUrl = details.url.replace(pattern, to);
             if (redirectUrl != details.url) {
-
-                return {redirectUrl: redirectUrl};
+                
+                return {
+                    redirectUrl: redirectUrl
+                };
             }
         }
     }
-
+    
     return {};
+}
+
+function extractStatusCode(line) {
+    var match = line.match(/[^ ]* (\d{3}) (.*)/);
+    if (match) {
+        
+        return match[1];
+    } else {
+        
+        return undefined;
+    }
 }
